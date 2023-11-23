eb0ee29c2a03f2e3e525254203c10a9783452973
Darkhan Almaqyn <146783027+almaqyn@users.noreply.github.com>
2023-11-23 13:28:50 +0100

    Port keychron k2 pro to Vial

---
+++ keyboards/keychron/k2_pro/ansi/rgb/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x08, 0x1F, 0xFC, 0x6F, 0x19, 0x86, 0x82, 0x19}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
